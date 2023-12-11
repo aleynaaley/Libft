e7d2d12631f60d813922c760f5cc3f18c55287e4
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2023-12-11 09:11:53 +0300

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -4,11 +4,34 @@
 // kullandigin fonksiyonlarin prototiplerini yaz 
 //kutuphanenin icerigini tanimliyoruz 
 
-char	ft_toupper(char ch);
-int	ft_tolower(char ch);
-char    *strrchr(char *str, int chr );     //typelari yazmak yeterli
+  //typelari yazmak yeterli
 
+int ft_tolower(int c);
+int	strncmp(const char *s1, const char *s2, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+int    isprint(int c);
+int isdigit(int c);
+int isascii(int c);
+int	isalpha(int c);
+int isalnum(int c);
+int	atoi(const char *str);
 
+char *strdup(const char *s1);
+char    *strrchr(const char *str, int chr );
+char    *strnstr(const char *haystack, const char *needle, size_t len);
+char	ft_toupper(int c);
+char    *strchr(const char *s, int c);
+
+void   *memset(void *b, int c, size_t len);
+void    *memmove(void *dst, const void *src, size_t len);
+void    *memcpy(void *restrict dst, const void *restrict src, size_t n);
+void    *memchr(const void *s, int c, size_t n);
+void    *calloc(size_t count, size_t size);
+void   bzero(void *s, size_t n);
+
+size_t strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	strlen(const char *s); 
+size_t	strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 
 
 #endif                     //bitis
