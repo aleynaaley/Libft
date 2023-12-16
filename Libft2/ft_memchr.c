5abc2b5be97219828542cc08a48f4fa461529964
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2023-12-16 23:06:52 +0300

    Update ft_memchr.c

--- Libft2/ft_memchr.c
+++ Libft2/ft_memchr.c
@@ -10,11 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void    *ft_memchr(const void *s, int c, size_t n){
+void    *ft_memchr(const void *s, int c, size_t n)
+{
     size_t	i;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{
