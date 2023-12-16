a6c62077c4a30a7e2b2c0a22733b4a8824826ab3
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2023-12-16 23:07:36 +0300

    Update ft_memcpy.c

--- Libft2/ft_memcpy.c
+++ Libft2/ft_memcpy.c
@@ -13,9 +13,10 @@
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n){
     size_t	i;
 
-	if (!dst)
-		return (NULL);
 	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
