51e2e7b8f563206c951e659b212248647ee323e3
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-01-05 20:48:27 +0300

    Update ft_memchr.c

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -14,16 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+    size_t			i;
+	unsigned char	*ptr;
 
-	if (!s)
-		return (0);
 	i = 0;
+    ptr = (unsigned char *)s;
 	while (i < n)
 	{
-		if (*(unsigned char *)s == (unsigned char)c)
-			return ((void *)s);
-		++s;
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)ptr + i);
 		i++;
 	}
 	return (0);
