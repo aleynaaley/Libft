0646672850541621dbb633dce4a6570854be9c57
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-01-12 12:56:39 +0300

    Update ft_strnstr.c

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -14,19 +14,20 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	unsigned int	i;
-	unsigned int	a;
+	size_t	i;
+	size_t	j;
 
-	a = ft_strlen(needle);
-	if (a == 0)
-		return ((char *)haystack);
-	if (len == 0)
-		return (0);
 	i = 0;
-	while (i <= (len - a) && haystack[i] != '\0')
+	j = 0;
+	if (!needle[0])
+		return ((char *)&haystack[i]);
+	while (haystack[i] && i < len)
 	{
-		if (ft_strncmp(&haystack[i], needle, a) == 0)
+		while (haystack[i + j] == needle[j] && needle[j] && (i + j) < len)
+			j++;
+		if (!needle[j])
 			return ((char *)&haystack[i]);
+		j = 0;
 		i++;
 	}
 	return (0);
