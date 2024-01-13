0e4a31992041c41775795e1697a51c917898aad5
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-01-13 12:47:06 +0300

    Update ft_putchar_fd.c

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -1,13 +1,7 @@
 
-
 #include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
-}
-int main (){
-    fd = open("a.txt",O_CREAT | O_RDWR ,0777);
-    ft_putchar_fd('a',fd);
-    close(fd);
 }
