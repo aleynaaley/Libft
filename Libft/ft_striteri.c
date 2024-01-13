6f56c5f68fb915a181e00b9471ba0546f5e4499f
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-01-13 12:56:42 +0300

    Update ft_striteri.c

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -14,16 +14,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
-
-}
-char aley(unsigned int index,char a){
-    a += 1;
-    return (a); 
-}
-
- int main(){
-    char *result;
-    char v[]= "aley";
-    result = ft_striteri(v,aley);
-    printf("%s", result);
-}
